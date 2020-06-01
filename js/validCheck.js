e0a6840c631ce9ffbf668fbb826a2c12fe0dfcce
Your Name <you@example.com>
2020-06-02 01:27:26 +0900

    수정중

--- js/validCheck.js
+++ js/validCheck.js
@@ -3,7 +3,7 @@ $(function(){
         var textTotal = $('input[type=text]').val();
         var testDetails = $('textarea#testDetails').val();
         console.log(testDetails);
-        if(textTotal&&!testDetails) {
+        if(!textTotal&&!testDetails) {
             alert("입력해.")
             location.href = '#test1'
         }
